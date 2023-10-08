475248910b4b578809dff4cf98c0ba1ad009d70b
Ethan <u6368029@anu.edu.au>
2023-10-08 21:03:49 +1100

    Day1 Completed

--- src/day1.rs
+++ src/day1.rs
@@ -1,8 +1,8 @@
-pub fn input(filepath: String) -> Vec<Vec<String>> {
+pub fn input(filepath: String) -> Vec<Vec<i32>> {
     let contents = std::fs::read_to_string(filepath)
         .expect("[ERROR] Should have been able to read file");
-    let mut return_vec: Vec<Vec<String>> = Vec::new();
-    let mut build_vec: Vec<String> = Vec::new();
+    let mut return_vec: Vec<Vec<i32>> = Vec::new();
+    let mut build_vec: Vec<i32> = Vec::new();
     let split_input: Vec<&str> = contents.split("\n").collect();
     for i in split_input {
         if i == "\r" && build_vec.len() > 0 {
@@ -10,11 +10,31 @@ pub fn input(filepath: String) -> Vec<Vec<String>> {
             build_vec = Vec::new()
         }
         else {
-            build_vec.push(String::from(i));
+            let v: i32 = String::from(i).replace("\r","").parse().unwrap();
+            build_vec.push(v);
         }
     }
     if return_vec.len() > 0 {
         return_vec.push(build_vec);
     }
     return_vec
+}
+
+pub fn day1_p1(input: Vec<Vec<i32>>) -> i32 {
+    let x: Vec<i32> = input
+        .iter()
+        .map(|c: &Vec<i32>| c.iter().sum())
+        .collect();
+    let y: i32 = *x.iter().max().unwrap();
+    return y;
+}
+
+pub fn day1_p2(input:Vec<Vec<i32>>) -> i32 {
+    let mut x: Vec<i32> = input
+        .iter()
+        .map(|c: &Vec<i32>| c.iter().sum())
+        .collect();
+    x.sort();
+    x.reverse();
+    return x[0..3].iter().sum();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 mod day1;
 
 fn main() {
+    day_1();
+}
+
+fn day_1() {
+    println!("Day 1 -----------");
     let day1_input = day1::input(String::from("input/day1.txt"));
-    println!("{:?}", day1_input);
+    println!("P1: {}", day1::day1_p1(day1_input));
+    let day1_input = day1::input(String::from("input/day1.txt"));
+    println!("P2: {}", day1::day1_p2(day1_input));
+    println!("-----------------");
 }
 
 
